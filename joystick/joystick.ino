db162b63a52e1b1cb118610fe66a4284f93a1ba9
JBTmarel <a.orribjarnason@gmail.com>
2026-01-08 19:05:14 +0000

    shit works

--- joystick/joystick.ino
+++ joystick/joystick.ino
@@ -1,32 +1,45 @@
-n// Numbering from left to right when leftmost cables are ground and 5V (So first pair, i.e. left, is white)
-int WHITE1 = 10;
-int WHITE2 = 11;
-int ORAN1 = 13;
-int ORAN2 = 12;
-int RED1 = 9;
-int RED2 = 8;
-int BROWN1 = 7;
-int BROWN2 = 6;
+#include <Encoder.h>
 
-string colors[8] = {BROWN2, BROWN1, RED2, RED1, WHITE1, WHITE2, ORAN2, ORAN1};
+// Numbering from left to right when leftmost cables are ground and 5V (So first pair, i.e. left, is white)
+int WHITE1 = 9;
+int WHITE2 = 10;
+int ORAN1 = 12;
+int ORAN2 = 11;
+int RED1 = 3; //opposite order
+int RED2 = 8; //opposite order
+int BROWN1 = 2; //opposite order
+int BROWN2 = 7; //opposite order
 
-int lastStates[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+Encoder xAxis(RED1, RED2);
+Encoder yAxis(BROWN1, BROWN2);
+Encoder spinner(ORAN1, ORAN2);
+Encoder button(WHITE1, WHITE2);
+
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 }
+long positionX = -999;
+long positionY = -999;
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  for (int i=6; i<14; i++) {
-    int state = digitalRead(i);
-    if (state != lastStates[i-6]) {
-      Serial.print("Got something on ");
-      Serial.println(colors[i-6]);
-    }
-    lastStates[i-6] = state;
-    delay(10);
+  long newX, newY;
+  newX = xAxis.read();
+  newY = yAxis.read();
+  if (newX != positionX || newY != positionY) {
+    Serial.print("X: ");
+    Serial.print(newX);
+    Serial.print(", Right: ");
+    Serial.print(newY);
+    Serial.println();
+    positionX = newX;
+    positionY = newY;
   }
-  
+  if (Serial.available()) {
+    Serial.read();
+    Serial.println("Reset both knobs to zero");
+    xAxis.write(0);
+    yAxis.write(0);
+  }
 }
