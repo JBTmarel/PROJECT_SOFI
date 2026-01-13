e210b36104be357b03c9ed57db621e859e1df080
JBTmarel <a.orribjarnason@gmail.com>
2026-01-13 22:00:20 +0000

    incomplete steering

--- joystick/joystick.ino
+++ joystick/joystick.ino
@@ -1,72 +1,137 @@
 #include <Encoder.h>
 
-// Numbering from left to right when leftmost cables are ground and 5V (So first pair, i.e. left, is white)
+// Numbering from left to right when leftmost cables are ground and 5V (So first
+// pair, i.e. left, is white)
 int WHITE1 = 9;
 int WHITE2 = 10;
 int ORAN1 = 12;
 int ORAN2 = 11;
-int RED1 = 3; //opposite order
-int RED2 = 8; //opposite order
-int BROWN1 = 2; //opposite order
-int BROWN2 = 7; //opposite order
+int RED1 = 3;
+int RED2 = 8;
+int BROWN1 = 2;
+int BROWN2 = 7;
 
-int throttlePin = 5;
+//int throttlePin = 5;
+int leftThrottlePin = 5; //VELJA PINS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+int rightThrottlePin = 6;
 
 Encoder xAxis(RED1, RED2);
 Encoder yAxis(BROWN1, BROWN2);
-Encoder spinner(ORAN1, ORAN2);
-Encoder button(WHITE1, WHITE2);
+// Encoder spinner(ORAN1, ORAN2);
+// Encoder button(WHITE1, WHITE2);
 
+int minV = 1.7; //Throttle output voltage when joystick is at 0, 0
+int maxV = 3; //Throttle output voltage when joystick is at 0, MAX
+int maxSteps = 68;
+float k_eq = ((255.0f*maxV/5.0f)-255.0f*minV/5.0f)/(maxSteps);
+float h_eq = 255.0f*minV/5.0f;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(throttlePin, OUTPUT);
+  //pinMode(throttlePin, OUTPUT);
+  pinMode(leftThrottlePin, OUTPUT);
+  pinMode(rightThrottlePin, OUTPUT);
 }
 
 float throttlef = 0.0;
 long positionX = -999;
 long positionY = -999;
-long posSpinner = -999;
+
 long throttle = 0;
+float leftThrottlef = 0;
+float rightThrottlef = 0;
+float ratio = 0
+
+
+long start = 0;
+bool lastMoveTrue = 0;
+int resetError = 3;
+
+long calcY;
+long calcX;
 
 void loop() {
-  long newX, newY, newSpinner;
+  long newX, newY;
   newX = xAxis.read();
-  newY = yAxis.read();
-  newSpinner = spinner.read();
-  if (newX != positionX || newY != positionY || newSpinner != posSpinner) {
+  newY = yAxis.read();  
+  if (newX != positionX || newY != positionY) {
     Serial.print("X: ");
     Serial.print(newX);
-    Serial.print(", Right: ");
+    Serial.print(", Y: ");
     Serial.print(newY);
-    // Serial.print(", Spinner: ");
-    // Serial.print(newSpinner);
-    //Serial.println();
     positionX = newX;
     positionY = newY;
-    posSpinner = newSpinner;
-    throttlef = (newX / 68.0f) * 255.0f;
-    //throttlef = (96.9f*newX+3814.8f)/68.0f;
-    throttle = lroundf(throttlef);
-    if (throttle < 0){
-      throttle = 0;
-      Serial.print(" THROTTLE_ZERO ");
-    }
-    // else if (throttle > 255){
+    // throttlef = (newX / 68.0f) * 255.0f;
+    // throttlef = (135.0f*newX+8160.0f)/68.0f;
+    //throttlef = (15.0f * newY + 1530.0f) / 17.0f;
+    //throttle = lroundf(throttlef);
+    // if (throttle < 0) {
+    //   throttle = 0;
+    //   Serial.print(" THROTTLE_ZERO ");
+    // } else if (throttle > 255) {
     //   throttle = 255;
+    // analogWrite(throttlePin, throttle);
+    // Serial.print(", Throttle:");
+    // Serial.print(throttle);
+    // Serial.println();
+
+    //}
+    calcY = newY;
+    calcX = newX;
+    if (calcY < 0) {
+      calcY = 0;
+      calcX = 0;
+    }
+    // if (calcX > 0) {
+    //   //rightThrottle = (15.0f * calcY + 1530.0f) / 17.0f;
+    //   rightThrottle = k_eq*calcY + h_eq;
+    //   //leftThrottle = (15.0f * abs(calcY-calcX) + 1530.0f) / 17.0f;
+    //   leftThrottle = k_eq*abs(calcY-calcX)+h_eq;
     // }
-    
-    analogWrite(throttlePin, throttle);
-    Serial.print(", Throttle:");
-    Serial.print(throttle);
+    // else if (calcX < 0) {
+    //   //leftThrottle = (15.0f * calcY + 1530.0f) / 17.0f;
+    //   leftThrottle = k_eq*calcY + h_eq;
+    //   //rightThrottle = (15.0f * abs(calcY+calcX) + 1530.0f) / 17.0f;
+    //   rightThrottle = k_eq*abs(calcY+calcX) + h_eq;
+    // }
+    if (calcX < 0) {
+      ratio = calcX/maxSteps;
+      speed = 
+      leftThrottlef = 
+    }
+
+    analogWrite(leftThrottlePin, lroundf(leftThrottlef));
+    analogWrite(rightThrottlePin, lroundf(rightThrottle));
+
+    Serial.print(", ThrottleL: ");
+    Serial.print(leftThrottle);
+    Serial.print(", ThrottleR: ");
+    Serial.print(rightThrottle);
     Serial.println();
+    start = millis();
+    lastMoveTrue = 1;
   }
-  if (Serial.available()) {
-    Serial.read();
-    Serial.println("Reset all to zero");
+  if (
+    ((millis() - start) > 3000) && 
+    (-1*resetError <= newX) &&
+    (newX <= resetError) && 
+    (-1*resetError < newY) &&
+    (newY < resetError) &&
+    lastMoveTrue) {
+
     xAxis.write(0);
     yAxis.write(0);
-    spinner.write(0);
+    // spinner.write(0);
+    newX = 0;
+    newY = 0;
+    Serial.println("Reset axes to zero");
+    Serial.print("X: ");
+    Serial.print(newX);
+    Serial.print(", Y: ");
+    Serial.print(newY);
+    start = millis();
+    lastMoveTrue = 0;
   }
 }
+
